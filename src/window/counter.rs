5a2eb79067691154a83b4828434aae21880c7854
joseph <joebeck03@icloud.com>
2023-05-23 01:53:28 +0100

    switch to iced

---
+++ src/window/counter.rs
@@ -0,0 +1,41 @@
+use iced::widget::{button, column, text, Column};
+
+pub struct Counter {
+    // The counter value
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `IncrementPressed` message when pressed
+            button("+").on_press(Message::IncrementPressed),
+
+            // We show the value of the counter here
+            text(self.value).size(50),
+
+            // The decrement button. We tell it to produce a
+            // `DecrementPressed` message when pressed
+            button("-").on_press(Message::DecrementPressed),
+        ]
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+}
